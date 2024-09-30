862c5b16b4776472fd60c1b0c9164bc1837f43f7
Mahendra Dewangan <98692513+mahendraDew@users.noreply.github.com>
2024-09-30 11:51:28 +0530

    Time: 0 ms (100%), Space: 42.4 MB (39.78%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -3,7 +3,7 @@ class Solution {
         return powerFun(x, (long)n);
     }
     public double powerFun(double x, long n){
-        if(n == 0){
+       if(n == 0){
             return 1;
         }
         if(n<0){
