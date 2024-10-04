c372c32f755a0fe3d58c5b7dfd6382d870fabc8b
Mahendra Dewangan <98692513+mahendraDew@users.noreply.github.com>
2024-10-04 12:15:29 +0530

    Time: 0 ms (100%), Space: 39.9 MB (96.07%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,8 +1,27 @@
 class Solution {
-    public int fib(int n) {
+
+    // fibo series using recursion
+    public int fibRec(int n) {
           if(n < 2){
             return n;
         }
         return fib(n-1) + fib(n-2);
+    }
+
+    //iterative approach
+    public int fib(int n) {
+        if(n == 0|| n == 1){
+            return n;
+        }
+        int a = 0;
+        int b = 1;
+        int c = 0;
+        for(int i = 2; i<=n; i++){
+            c = a+b;
+            a = b;
+            b = c;
+        }
+
+        return c;
     }
 }
