3c574d058a713672ef34921e5088d26cf562abcc
Mahendra Dewangan <98692513+mahendraDew@users.noreply.github.com>
2024-10-15 14:23:20 +0530

    Time: 0 ms (100%), Space: 42.8 MB (91.46%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,36 +1,30 @@
 class Solution {
-    public boolean  search(int[] arr, int target) {
-         int s = 0;
-        int e = arr.length - 1;
-        while (s <= e){
-            int m = s + (e - s)/2;
-            if(arr[m] == target){
+    public boolean search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(nums[mid] == target){
                 return true;
             }
-            if(arr[s] < arr[m]){
-                if(target >= arr[s] && target <= arr[m]){
-                    e = m - 1;
-                }
-                else{
-                    s = m + 1;
+            if(nums[start]<nums[mid]){
+                if(target >= nums[start] && target <= nums[mid]){
+                    end = mid-1;
+                }else{
+                    start = mid+1;
                 }
             }
-            else if(arr[s] > arr[m]){
-                if(target > arr[m] && target <= arr[e]){
-                    s = m + 1;
-                }
-                else {
-                    e = m - 1;
+            else if(nums[start] > nums[mid]){
+                if(target > nums[mid] && target <= nums[end]){
+                    start = mid+1;
+                }else{
+                    end = mid-1;
                 }
             }
             else{
-                s++;
+                start++;
             }
-
-
         }
         return false;
     }
-
-
 }
