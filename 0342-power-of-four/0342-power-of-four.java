adab7ca98519f2697427ebc7e81a3b7c8fb6ce75
Mahendra Dewangan <98692513+mahendraDew@users.noreply.github.com>
2024-10-16 12:19:35 +0530

    Time: 0 ms (100%), Space: 41.2 MB (10.15%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,19 +1,21 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if(n == 1){
+       if (n == 0) {
+            return false;
+        }
+        if(n==1){
             return true;
         }
-        while(n > 0){
-            if(n%4 != 0){
-                return false;
-            }
-            n = n/4;
-             if(n == 1){
-                return true;
-            }
-        }
-
-        return false;
-        
+        int x = n;
+        return isPowerOfFourFun(n, x);
     }
+    public static boolean isPowerOfFourFun(int n, int x){
+        if(n%4 ==0 && x%4 == 0 && n==4){
+            return true;
+        }if(n%4!=0){
+            return false;
+        }
+        return isPowerOfFourFun(n/4, x);
+    }
+
 }
