2d1af982c5978f8525a408c0ee969622086e799a
Mahendra Dewangan <98692513+mahendraDew@users.noreply.github.com>
2024-11-20 10:14:51 +0530

    Time: 0 ms (100%), Space: 41 MB (83.85%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -1,33 +1,40 @@
 class Solution {
     public int splitArray(int[] nums, int k) {
-        //find lower and upper limit
-        int start = 0;
-        int end = 0;
-        for(int i = 0; i<nums.length; i++){
-            start = Math.max(start, nums[i]);
-            end += nums[i];
+       int low = nums[0];
+        int high = 0;
+        //find maximum and summation:
+        for (int i = 0; i < nums.length; i++) {
+            low = Math.max(low, nums[i]);
+            high += nums[i];
         }
 
-        //apply bsearch
-        while(start<=end){
-            int mid = start + (end-start)/2;
-            int sum =0;
-            int pc = 1;  //piece counter 
-            for(int i = 0; i<nums.length; i++){
-                if(nums[i]+sum>mid){
-                    sum = nums[i];
-                    pc++;
-                }else{
-                    sum = sum + nums[i];
-                }
-            }
-            if(pc > k){
-                start = mid+1;
-            }else{
-                end = mid-1;
+        //Apply binary search:
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int partitions = countPartitions(nums, mid);
+            if (partitions > k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
-
-        return start;
+        return low;
     }
+     public static int countPartitions(int[] a, int maxSum) {
+        int n = a.length; //size of array.
+        int partitions = 1;
+        long subarraySum = 0;
+        for (int i = 0; i < n; i++) {
+            if (subarraySum + a[i] <= maxSum) {
+                //insert element to current subarray
+                subarraySum += a[i];
+            } else {
+                //insert element to next subarray
+                partitions++;
+                subarraySum = a[i];
+            }
+        }
+        return partitions;
+    }
+
 }
