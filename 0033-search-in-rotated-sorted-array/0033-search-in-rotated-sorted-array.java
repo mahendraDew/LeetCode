233d56e68ace192a0b6598cc7e893a7b7a36b9d1
Mahendra Dewangan <98692513+mahendraDew@users.noreply.github.com>
2024-12-14 11:04:42 +0530

    Time: 0 ms (100%), Space: 41.9 MB (63.37%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -3,40 +3,44 @@ class Solution {
         int pivot = findPivot(nums);
         if(pivot == -1){
             return bsearch(nums, target, 0, nums.length-1);
-        }else if(nums[pivot] == target){
-            return pivot;
-        }else if(nums[0] <= target){
-            return bsearch(nums, target, 0, pivot-1);
         }
-        return bsearch(nums, target, pivot+1, nums.length-1);
+        else if(nums[pivot] == target){
+            return pivot;
+        }
+        else if(target >= nums[0]){
+            return bsearch(nums, target, 0, pivot-1);
+        }else{
+            return bsearch(nums, target, pivot+1, nums.length-1);
+        }
     }
-    private int findPivot(int[] arr){
-        int start = 0;
+    public int findPivot(int[] arr){
+        int start = 0; 
         int end = arr.length-1;
         while(start<end){
-            int mid = start + (end-start)/2;
-            if(mid < end && arr[mid] > arr[mid+1]){
+            int mid = start + (end - start)/2;
+            if(mid < end && arr[mid]>arr[mid+1]){
                 return mid;
             }
-            if(mid > start && arr[mid] < arr[mid-1]){
+            if(mid>start && arr[mid]<arr[mid-1]){
                 return mid-1;
-            }if(arr[mid] >= arr[start]){
+            }
+            if(arr[mid] >= arr[start]){
                 start = mid+1;
-            }else {
+            }else{
                 end = mid-1;
             }
         }
         return -1;
     }
-    private int bsearch(int[] arr, int target, int start, int end){
-        while(start <= end){
-            int mid = start + (end-start)/2;
-            if(arr[mid]==target){
+    public int bsearch(int[] arr, int target, int start, int end){
+        while(start<=end){
+            int mid = start + (end - start)/2;
+            if(arr[mid] == target){
                 return mid;
-            }else if(arr[mid]<target){
-                start = mid+1;
             }else if(arr[mid] > target){
                 end = mid-1;
+            }else{
+                start = mid+1;
             }
         }
         return -1;
