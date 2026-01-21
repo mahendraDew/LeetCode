4ca2dedf574365ac1e5e492e81234ad1147171e5
Mahendra Dewangan <98692513+mahendraDew@users.noreply.github.com>
2026-01-21 23:15:56 +0530

    Time: 3 ms (13.53%), Space: 44 MB (6.99%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,18 +1,5 @@
 class Solution {
     public void sortColors(int[] nums) {
-         int[] arr = new int[3];
-        for(int i = 0; i<nums.length; i++){
-            arr[nums[i]]++;
-        }
-        System.out.println(Arrays.toString(arr)); 
-        // int i = 0;
-        int index = 0;
-        for(int i = 0; i<arr.length; i++){
-            while(arr[i] > 0){
-                nums[index++] = i;
-                arr[i]--;
-            }
-        }
-        
+        Arrays.sort(nums);
     }
 }
